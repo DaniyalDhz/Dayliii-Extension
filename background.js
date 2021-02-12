22a6de124647fffc54e6066f9a14bcdd938ecb09
daniyaldhz <daniyaldehleh@gmail.com>
2021-02-12 17:18:14 -0500

    'added everything'

---
+++ background.js
@@ -0,0 +1,98 @@
+let tabId;
+
+chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+    console.log(tabs[0].id)
+})
+// fetch('http://127.0.0.1:5000/current', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//         email: '@gmail.com', //sample data
+//         id: '2' //sample data
+//     }),
+//     headers: {
+//         'Content-Type': 'application/json;charset=UTF-8',
+//         Accept: 'application/json'
+//     }
+// })
+
+//stores value upon change
+chrome.storage.onChanged.addListener(function(changes, storageName) {
+    let timestamp = changes.time.newValue;
+    var sec_num = parseInt(timestamp, 10); // don't forget the second param //parseInt is same as int()
+    var hours = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours < 10) { hours = "0" + hours; } //for display purposes
+    if (minutes < 10) { minutes = "0" + minutes; }
+    if (seconds < 10) { seconds = "0" + seconds; }
+    let formattedTime = hours + ':' + minutes;
+    chrome.browserAction.setBadgeText({ "text": formattedTime })
+}) //this function looks to be for the badge (icon timer) display and functionality
+
+let countup2;
+let countup3;
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.cmd == "start") {
+            countup2 = setInterval(() => {
+                getDB('time', function(opt) {
+                    var timeup = opt.time
+
+                    if (timeup == startTime) {
+                        playsound()
+                        chrome.tabs.sendMessage(tabId, { cmd: "popup" })
+                        console.log('popup to tab ', tabId)
+                        clearInterval(countup2)
+                    } else {
+                        setDB('time', opt.time + 1)
+                    }
+
+                })
+            }, 1000);
+        }
+
+        if (request.cmd == "extend") {
+            countup3 = setInterval(() => {
+                getDB('time', function(opt) {
+                    setDB('time', opt.time + 1)
+                })
+            }, 1000);
+            console.log(request.cmd)
+        }
+
+
+        if (request.cmd == "stop") {
+            clearInterval(countup2)
+            clearInterval(countup3)
+            setDB('time', 0)
+            setDB('extended', false)
+        }
+
+    }
+);
+
+function setDB(key, value) {
+    chrome.storage.sync.set({
+        [key]: value
+    });
+
+}
+
+function getDB(key, cb) {
+    chrome.storage.sync.get(key, (opt) => {
+        cb(opt)
+    });
+
+}
+
+function playsound() {
+    console.log('play sound')
+    var audio = new Audio('https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-61905/zapsplat_bells_small_hand_bell_ring_in_water_weird_cartoon_tone_001_61906.mp3');
+    audio.play();
+}
+
+chrome.tabs.onActiveChanged.addListener((newid) => {
+    console.log('change tab to ', newid)
+    tabId = newid;
+})
---
+++ script.js
@@ -0,0 +1,146 @@
+var countup;
+var _clock;
+var storeTime;
+var elementStart = document.getElementById('start')
+var startTime = 65; // in second
+
+// load db with 'alert' key
+getDB('alert', (data) => {
+    var txt = document.getElementById("enter");
+    // if the data exist, then insert it the input value
+    if (data.alert) {
+        txt.value = data.alert;
+    }
+})
+
+
+// get time from db
+getDB('time', (data) => {
+    storeTime = data.time;
+    if (storeTime > 0) {
+        elementStart.innerHTML = 'Extend'; // show extend button if time > 0
+        _clock = $('.clock').FlipClock(storeTime, {
+            clockFace: 'DailyCounter',
+            showSeconds: false,
+            countdown: false
+        });
+    } else {
+        _clock = $('.clock').FlipClock(0, {
+            clockFace: 'DailyCounter',
+            showSeconds: false,
+            countdown: false,
+            autoStart: false
+        });
+    }
+
+    // get extended from db
+    getDB('extended', function(opt) {
+        // if stored time has reach starttime
+        // and if extended is false then stop the timer on the popup
+        if (data.time == startTime && opt.extended == false) {
+            _clock.stop()
+        }
+    })
+})
+
+
+
+
+elementStart.addEventListener('click', function(e) {
+
+    let buttonname = e.target.innerHTML;
+    if (buttonname == 'Start') {
+        e.target.innerHTML = 'Extend'
+
+        // send start to background.js
+        chrome.runtime.sendMessage({ cmd: "start" });
+
+
+
+        _clock = $('.clock').FlipClock(startTime, {
+            clockFace: 'DailyCounter',
+            showSeconds: false,
+            countdown: false,
+            autoStart: false
+        });
+        _clock.setTime(startTime);
+
+
+        setTimeout(() => {
+            _clock.setTime(0);
+            _clock.start();
+            countup = setInterval(function() {
+
+                var timeup = _clock.getTime().time;
+                if (timeup == startTime) {
+                    // play sound, and show popup
+
+                    _clock.stop();
+                    clearInterval(countup)
+                } else {
+                    setDB('time', _clock.getTime().time)
+                }
+
+            }, 1000);
+        }, 2000);
+    } else {
+        chrome.runtime.sendMessage({ cmd: "extend" });
+        setDB('Extended', true)
+
+        countup = setInterval(function() {
+            _clock.start()
+            setDB('time', _clock.getTime().time)
+
+        }, 1000);
+    }
+});
+
+var elementStop = document.getElementById("stop");
+elementStop.addEventListener('click', function() {
+
+    var txt = document.getElementById("enter").value;
+    if (txt.length > 0) {
+        elementStart.innerHTML = 'Start'
+
+        chrome.runtime.sendMessage({ cmd: "stop" });
+        setDB('time', 0)
+        setDB('Extended', false)
+        clearInterval(countup);
+        _clock.stop();
+        _clock = $('.clock').FlipClock(0, {
+            clockFace: 'DailyCounter',
+            showSeconds: false,
+            countdown: false,
+            autoStart: false
+        });
+    }
+
+});
+
+document.getElementById("enter").addEventListener('keyup', function(e) {
+    setDB('alert', e.target.value)
+});
+
+
+function extend(timespan) {
+    //should extend the timer from the timespan that it stopped
+    _clock = $('.clock').FlipClock(timespan, {
+        clockFace: 'DailyCounter',
+        showSeconds: false,
+        countdown: false,
+    });
+}
+
+
+function setDB(key, value) {
+    chrome.storage.sync.set({
+        [key]: value
+    });
+
+}
+
+function getDB(key, cb) {
+    chrome.storage.sync.get(key, (opt) => {
+        cb(opt)
+    });
+}
