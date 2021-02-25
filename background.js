4f19eab9795f1a7704e34c4a5b00e7ac75297764
Khoir359 <khoir359@gmail.com>
2021-02-25 09:23:48 +0700

    fixed timer

--- background.js
+++ background.js
@@ -23,6 +23,7 @@ let countup3; //for extend
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.cmd == "start") { //comes from chrome.tabs.sendmessage(tabid{cmd:'start'}). sends message sends an object.
+            setDB('running', true)
             countup2 = setInterval(() => {
                 getDB('time', function(opt) { //opt is a data callback from the database when you use getDB(key, callback)
                     var timeup = opt.time
@@ -32,17 +33,20 @@ chrome.runtime.onMessage.addListener(
                         chrome.tabs.sendMessage(tabId, { cmd: "popup" }) //? what is cmd?
                         console.log('popup to tab ', tabId)
                         clearInterval(countup2)
+                        setDB('running', false)
                     } else {
+
                         setDB('time', opt.time + 1) //? why adding + 1?
                     }
 
                 })
             }, 1000); //every 1000ms, it will check if timeup == startTime or not
-			current();
-			
+            current();
+
         }
 
         if (request.cmd == "extend") {
+            setDB('running', true)
             countup3 = setInterval(() => {
                 getDB('time', function(opt) {
                     setDB('time', opt.time + 1)
@@ -53,17 +57,18 @@ chrome.runtime.onMessage.addListener(
 
 
         if (request.cmd == "stop") {
+            setDB('running', false)
             clearInterval(countup2) //resets time
             clearInterval(countup3) //resets extension time
             setDB('time', 0)
             setDB('extended', false)
         }
 
-		if (request.cmd == "alert") {
-			getDB('alert', function(database) {
-				console.log('alert value is ', database.alert)		
-				let eventName = database.alert
-				submit(eventName)
+        if (request.cmd == "alert") {
+            getDB('alert', function(database) {
+                console.log('alert value is ', database.alert)
+                let eventName = database.alert
+                submit(eventName)
             })
         }
 
@@ -79,7 +84,7 @@ function setDB(key, value) {
 
 function getDB(key, cb) { //cb is callback
     chrome.storage.sync.get(key, (opt) => {
-        cb(opt) 
+        cb(opt)
     });
 
 }
@@ -98,87 +103,85 @@ chrome.tabs.onActiveChanged.addListener((newid) => {
 
 //Server  API codes
 function submit(eventName) {
-	console.log('submitted')
-	console.log('User inputted ' + eventName + ' from submit')
-	
-	chrome.identity.getProfileUserInfo(function (userInfo) {
-		const userEmail = userInfo.email
-		calName = document.getElementById('cars') // name of calendar (for goals)
-		// let eventName = document.getElementById('enter').onclick; //eventName
-		fetch('http://127.0.0.1:5000/execute', {
-				method: 'POST',
-				body: JSON.stringify({
-					email: userInfo.email,
-					// callName: 'Personal Finance',
-					eventName: eventName
-				}),
-				headers: {
-					'Content-Type': 'application/json;charset=UTF-8',
-					Accept: 'application/json'
-				}
-			})
-			.then((response) => response.json())
-			.then(function (json) {
-				if (json.message==='daily limit of 5 reached. Subscribe for unlimited access'){ //FIXME: sentence should not be hardcoded
-					console.log('daily limit of 5 reached. Subscribe for unlimited access')			
-				}
-				else{
-					chrome.storage.local.set({executeResponse: json.message})
-				}
-			})
-			.catch(console.log('didnt receive data ' + response)) // add err in function
-	})
+    console.log('submitted')
+    console.log('User inputted ' + eventName + ' from submit')
+
+    chrome.identity.getProfileUserInfo(function(userInfo) {
+        const userEmail = userInfo.email
+        calName = document.getElementById('cars') // name of calendar (for goals)
+        // let eventName = document.getElementById('enter').onclick; //eventName
+        fetch('http://127.0.0.1:5000/execute', {
+                method: 'POST',
+                body: JSON.stringify({
+                    email: userInfo.email,
+                    // callName: 'Personal Finance',
+                    eventName: eventName
+                }),
+                headers: {
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    Accept: 'application/json'
+                }
+            })
+            .then((response) => response.json())
+            .then(function(json) {
+                if (json.message === 'daily limit of 5 reached. Subscribe for unlimited access') { //FIXME: sentence should not be hardcoded
+                    console.log('daily limit of 5 reached. Subscribe for unlimited access')
+                } else {
+                    chrome.storage.local.set({ executeResponse: json.message })
+                }
+            })
+            .catch(console.log('didnt receive data ' + response)) // add err in function
+    })
 }
 
 var views = chrome.extension.getViews({
-	type: 'popup'
+    type: 'popup'
 })
 for (var i = 0; i < views.length; i++) {
-	views[i].document
-		.getElementById('submit')
-		.addEventListener('click', submit)
-	console.log('loaded')
+    views[i].document
+        .getElementById('submit')
+        .addEventListener('click', submit)
+    console.log('loaded')
 }
 
 function current() { //possibly take out
-	// should be merged with start() func
-	console.log('current got hit')
-	chrome.identity.getProfileUserInfo(function (userInfo) {
-		console.log(JSON.stringify(userInfo))
-		const userEmail = userInfo.email
-		fetch('http://127.0.0.1:5000/current', {
-				method: 'POST',
-				body: JSON.stringify({
-					email: userEmail
-				}),
-				headers: {
-					'Content-Type': 'application/json;charset=UTF-8',
-					Accept: 'application/json'
-				}
-			})
-			.then((response) => response.json()) // this can prolly be taken out
-			.then(function (json) {
-				chrome.storage.local.set({
-					currentEvent: json.event
-				})
-				chrome.storage.local.set({
-					list: json.list
-				})
-				chrome.storage.local.set({
-					time: json.time
-				})
-				setDB('time',json.time) //needs to be in this format cause it's getting called as getDB('time') everywhere else
-				// document.getElementById('enter').onclick = json.event // name of event
-			})
-			.catch(console.log('didnt receive data')) // add err in function
-	})
+    // should be merged with start() func
+    console.log('current got hit')
+    chrome.identity.getProfileUserInfo(function(userInfo) {
+        console.log(JSON.stringify(userInfo))
+        const userEmail = userInfo.email
+        fetch('http://127.0.0.1:5000/current', {
+                method: 'POST',
+                body: JSON.stringify({
+                    email: userEmail
+                }),
+                headers: {
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    Accept: 'application/json'
+                }
+            })
+            .then((response) => response.json()) // this can prolly be taken out
+            .then(function(json) {
+                chrome.storage.local.set({
+                    currentEvent: json.event
+                })
+                chrome.storage.local.set({
+                    list: json.list
+                })
+                chrome.storage.local.set({
+                    time: json.time
+                })
+                setDB('time', json.time) //needs to be in this format cause it's getting called as getDB('time') everywhere else
+                // document.getElementById('enter').onclick = json.event // name of event
+            })
+            .catch(console.log('didnt receive data')) // add err in function
+    })
 }
 
 var views = chrome.extension.getViews({
-	type: 'popup'
+    type: 'popup'
 })
 for (var i = 0; i < views.length; i++) {
-	views[i].document.getElementById('start').addEventListener('click', current)
-	console.log('loaded')
+    views[i].document.getElementById('start').addEventListener('click', current)
+    console.log('loaded')
 }
-
